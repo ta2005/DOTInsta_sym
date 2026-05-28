1d50b819474bfb752a750a35500fef64fae5c29d
Talel Zighni <talelzigni24@gmail.com>
2026-05-28 18:48:25 +0100

    added the comments

---
+++ src/Controller/CommentaireController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Commentaire;
+use App\Entity\Post;
+use App\Form\CommentaireType;
+use App\Repository\CommentaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/commentaire')]
+final class CommentaireController extends AbstractController
+{
+    #[Route(name: 'app_commentaire_index', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function index(CommentaireRepository $commentaireRepository): Response
+    {
+        return $this->render('commentaire/index.html.twig', [
+            'commentaires' => $commentaireRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new/post/{id}', name: 'app_commentaire_new', methods: ['GET', 'POST'])]
+    public function new(Request $request,Post $post, EntityManagerInterface $entityManager): Response
+    {
+        $commentaire = new Commentaire();
+        $commentaire->setDateCreation(new \DateTime());
+        $commentaire->setPostId($post);
+        $commentaire->setAuteurId($this->getUser());
+        $this->denyAccessUnlessGranted('COMMENTAIRE_CREATE', $commentaire);
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_commentaire_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('commentaire/new.html.twig', [
+            'commentaire' => $commentaire,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_commentaire_show', methods: ['GET'])]
+    #[IsGranted('COMMENTAIRE_VIEW', subject: 'commentaire')]
+    public function show(Commentaire $commentaire): Response
+    {
+        return $this->render('commentaire/show.html.twig', [
+            'commentaire' => $commentaire,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_commentaire_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('COMMENTAIRE_EDIT', subject: 'commentaire')]
+    public function edit(Request $request, Commentaire $commentaire, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_commentaire_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('commentaire/edit.html.twig', [
+            'commentaire' => $commentaire,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_commentaire_delete', methods: ['POST'])]
+    #[IsGranted('COMMENTAIRE_DELETE', subject: 'commentaire')]
+    public function delete(Request $request, Commentaire $commentaire, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$commentaire->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($commentaire);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_commentaire_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Security/Voter/CommentaireVoter.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Post;
+use App\Repository\MembreGroupeRepository;
+use App\Entity\Groupe;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class CommentaireVoter extends Voter
+{
+    public const EDIT = 'COMMENTAIRE_EDIT';
+    public const VIEW = 'COMMENTAIRE_VIEW';
+    public const DELETE = 'COMMENTAIRE_DELETE';
+    public const CREATE = 'COMMENTAIRE_CREATE';
+
+    private $membreRepo;
+
+    public function __construct(MembreGroupeRepository $membreRepo){
+        $this->membreRepo=$membreRepo;
+    }
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::EDIT, self::VIEW,self::DELETE,self::CREATE])
+            && $subject instanceof \App\Entity\Commentaire;
+    }
+
+    private function isMembre($user,$groupe):bool{
+       $mem = $this->membreRepo->findOneBy([
+            'user_id' => $user,
+            'groupe_d' => $groupe,
+         ]);
+         return $mem !== null;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
+    {
+        $user = $token->getUser();
+
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            $vote?->addReason('The user must be logged in to access this resource.');
+
+            return false;
+        }
+        if (in_array('ROLE_ADMIN',$user->getRoles())) return true;
+
+        $comm = $subject;
+        $post = $comm->getPostId();
+        $groupe = $post->getGroupId();
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case self::EDIT:
+            case self::DELETE:
+                // logic to determine if the user can EDIT
+                // return true or false
+                return $comm->getAuteurId() === $user;
+                break;
+
+            //for both of these the user must be part of the groupe
+            case self::VIEW:
+            case self::CREATE:
+                // logic to determine if the user can VIEW
+                // return true or false
+                return $this->isMembre($user,$groupe);
+                break;
+        }
+
+        return false;
+    }
+
+}
