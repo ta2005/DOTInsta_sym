c10a3a91558f03f3d108f929864d31e68fcd96b7
Souhail-Bel <robotboy976@gmail.com>
2026-05-28 21:34:07 +0100

    Role-based PostController

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -20,12 +20,13 @@ final class PostController extends AbstractController
     #[IsGranted('ROLE_USER')]
     public function index(PostRepository $postRepository): Response
     {
-        //this one i need to make it use the user id
-        //to check for post
-        //or even remove it
-        return $this->render('post/index.html.twig', [
-            'posts' => $postRepository->findAll(),
-        ]);
+        $user = $this->getUser();
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $posts = $postRepository->findAll();
+        } else {
+            $posts = $postRepository->findFeedForUser($user);
+        }
+        return $this->render('post/index.html.twig', ['posts' => $posts]);
     }
 
     #[Route('/new/groupe/{id}', name: 'app_post_new', methods: ['GET', 'POST'])]
